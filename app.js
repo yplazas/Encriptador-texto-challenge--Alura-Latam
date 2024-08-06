fe571d70e217d305aaa506fdac141c258ee79c60
yplazas <yeialex9511@gmail.com>
2024-08-06 08:31:59 -0500

    Ajuste: Se termina el diseño del encriptador de texto y es totalmente responsivo

---
+++ app.js
@@ -0,0 +1,7 @@
+function condicionesIniciales() {
+    document.getElementById("resultadoParrafo").style.display = "none";
+    document.getElementById("resultadoBotonCopiar").style.display = "none";
+}
+
+
+condicionesIniciales();
